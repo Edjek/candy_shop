8f303d8c76f3f7f1bb81649909585773d0367f12
Edjek <edjek@hotmail.fr>
2024-07-16 16:19:34 +0200

    clean code

---
+++ src/Controller/Admin/ArticleController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin/article', 'admin_article_')]
+class ArticleController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('admin/article/index.html.twig', [
+            'controller_name' => 'ArticleController',
+        ]);
+    }
+
+    #[Route('/create', name: 'create')]
+    public function create(): Response
+    {
+        return $this->render('admin/article/create.html.twig', [
+            'controller_name' => 'ArticleController',
+        ]);
+    }
+
+}
